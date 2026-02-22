3b266b464b1175bc8a4bb6fdcc9b7e4abfebc2dd
Angelsarajayan <angelsarajayan7602@gmail.com>
2026-02-22 05:41:42 -0500

    Added EmotionDetection package

---
+++ EmotionDetection/emotion_detection.py
@@ -0,0 +1,49 @@
+import requests # import requests library to handle HTTP requests
+import json
+
+def emotion_detector(text_to_analyze):
+    # URL of the emotion detection
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+
+    # Create JSON with the text for emotional analysis
+    input_json = { "raw_document": { "text": text_to_analyze } }
+
+    # Set the headers required for the API request
+    header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+
+    # Send a POST request to the API with the text and headers 
+    response = requests.post(url, json=input_json, headers=header)
+
+    # Convert response text into dictionary
+    formatted_text = json.loads(response.text)
+
+    # Extract emotion scores
+    emotions = formatted_text["emotionPredictions"][0]["emotion"]
+
+    anger_score = emotions["anger"]
+    disgust_score = emotions["disgust"]
+    fear_score = emotions["fear"]
+    joy_score = emotions["joy"]
+    sadness_score = emotions["sadness"]
+
+    # Store emotions in dictionary
+    emotion_scores = {
+        "anger": anger_score,
+        "disgust": disgust_score,
+        "fear": fear_score,
+        "joy": joy_score,
+        "sadness": sadness_score
+    }
+
+    # Find dominant emotion
+    dominant_emotion = max(emotion_scores, key=emotion_scores.get)
+
+    # Return required output format
+    return {
+        "anger": anger_score,
+        "disgust": disgust_score,
+        "fear": fear_score,
+        "joy": joy_score,
+        "sadness": sadness_score,
+        "dominant_emotion": dominant_emotion
+    }
